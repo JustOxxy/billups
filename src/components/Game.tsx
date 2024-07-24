c94df8a773206d9ca1005e46e20fb358933d984d
Oksana Kukushkina <kksundel2019@gmail.com>
2024-07-24 19:04:39 +0200

    Add game board

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,1 +1,16 @@
-export const Game = () => {};
+import { ScoreBoard } from './ScoreBoard';
+import { useFetchChoicesQuery } from '../services/api';
+import { RecentResultsBoard } from './RecentResultsBoard';
+import { GameBoard } from './GameBoard';
+
+export const Game = () => {
+  const { data: choices, isLoading, isError, isSuccess } = useFetchChoicesQuery();
+
+  return (
+    <>
+      <ScoreBoard />
+      <GameBoard />
+      <RecentResultsBoard />
+    </>
+  );
+};
